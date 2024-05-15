afb0b17b0a3f0ac76bfb67e1c4aae89213b40eba
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2024-05-15 16:57:58 +0200

    Simple default test program

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -3,13 +3,18 @@ using GXPEngine;								// GXPEngine contains the engine
 
 public class MyGame : Game
 {
+	private readonly Sprite _sprite;	// Create a sprite from an image file
+
 	public MyGame() : base(800, 600, false)		// Create a window that's 800x600 and NOT fullscreen
 	{
+		_sprite = new Sprite("colors.png");
+		_sprite.SetXY(100, 100);
+		this.AddChild(_sprite);
 	}
 
 	void Update()
 	{
-		// Empty
+		_sprite.rotation += 0.1f;
 	}
 
 	static void Main()							// Main() is the first method that's called when the program is run
